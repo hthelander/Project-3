d3410d35ffa63aa146b402a795a36b97ffa2a0a3
hthelander <112781292+hthelander@users.noreply.github.com>
2023-01-11 01:17:21 -0500

    updated initial API call to get data for all national parks

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,54 +4,54 @@ var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 });
 
 
-// var layers = {
-//   PARKS: new L.LayerGroup(),
-//   CAMPGROUNDS: new L.LayerGroup(),
-//   LOW: new L.LayerGroup(),
-//   NORMAL: new L.LayerGroup(),
-//   OUT_OF_ORDER: new L.LayerGroup()
-// };
+var layers = {
+  PARKS: new L.LayerGroup(),
+  CAMPGROUNDS: new L.LayerGroup(),
+  LOW: new L.LayerGroup(),
+  NORMAL: new L.LayerGroup(),
+  OUT_OF_ORDER: new L.LayerGroup()
+};
 
 // Create the map with our layers.
 var map = L.map("map-id", {
   center: [40.73, -94.0059],
-  zoom: 4
-//   layers: [
-//     layers.PARKS,
-//     layers.CAMPGROUNDS,
-//     layers.AMENITIES,
-//     layers.PARKINGLOTS,
-//     layers.WEBCAMS
-//   ]
+  zoom: 4,
+  layers: [
+    layers.PARKS,
+    layers.CAMPGROUNDS,
+    layers.AMENITIES,
+    layers.PARKINGLOTS,
+    layers.WEBCAMS
+  ]
 });
 
 // Add our "streetmap" tile layer to the map.
 streetmap.addTo(map);
 
-// // Create an overlays object to add to the layer control.
-// var overlays = {
-//   "Parks": layers.PARKS,
-//   "Campgrounds": layers.CAMPGROUNDS,
-//   "Amenities": layers.AMENITIES,
-//   "Parking Lots": layers.PARKINGLOTS,
-//   "Webcam Views": layers.WEBCAMS
-// };
+// Create an overlays object to add to the layer control.
+var overlays = {
+  "Parks": layers.PARKS,
+  "Campgrounds": layers.CAMPGROUNDS,
+  "Amenities": layers.AMENITIES,
+  "Parking Lots": layers.PARKINGLOTS,
+  "Webcam Views": layers.WEBCAMS
+};
 
-// // Create a control for our layers, and add our overlays to it.
-// L.control.layers(null, overlays).addTo(map);
+// Create a control for our layers, and add our overlays to it.
+L.control.layers(null, overlays).addTo(map);
 
-// // Create a legend to display information about our map.
-// var info = L.control({
-//   position: "bottomright"
-// });
+// Create a legend to display information about our map.
+var info = L.control({
+  position: "bottomright"
+});
 
-// // When the layer control is added, insert a div with the class of "legend".
-// info.onAdd = function() {
-//   var div = L.DomUtil.create("div", "legend");
-//   return div;
-// };
-// // Add the info legend to the map.
-// info.addTo(map);
+// When the layer control is added, insert a div with the class of "legend".
+info.onAdd = function() {
+  var div = L.DomUtil.create("div", "legend");
+  return div;
+};
+// Add the info legend to the map.
+info.addTo(map);
 
 // Initialize an object that contains icons for each layer group.
 var icons = {
