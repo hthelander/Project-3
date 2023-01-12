36ca60d2388d9789c2d36bc6664278abbd121540
Colleen-Cobb <colleen.cobb5@gmail.com>
2023-01-12 16:34:51 -0500

    working on dashboard

--- static/js/activites.js
+++ static/js/activites.js
@@ -1,16 +1,29 @@
-const url = "http://127.0.0.1:5000/api/v1.0/parks"
+d3.json("http://127.0.0.1:5000/api/v1.0/parks").then((data)=>{
+    
+console.log(data);
 
-const dataPromise = d3.json(url)
-console.log("Data Promise: ", dataPrmoise);
+})
 
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log(data.names);
+// Call the data into the inspector console. 
+function init() {
+  d3.json("http://127.0.0.1:5000/api/v1.0/parks").then(function (data) {
+    console.log("http://127.0.0.1:5000/api/v1.0/parks", data);
+    // Set up the DropDown:
+    let DropDown = d3.select(`dropdown`);
 
-  let names = data.cost;
+    for (var index = 0; index < parks.length; index++) {
+      var park = parks[index];
 
-  let metadata = data.fullName;
+    //   DropDown.append(`option`).text(name).property(`value`, name);
+    // });
 
-  let samples = data.activity;
-});
 
+       data.names.forEach((name) => {
+        DropDown.append(`option`).text(park.fullName).property(`value`, park.fullName);
+    });
+    // Reset demographic info and visuals to first subject when page is refreshed.
+    const firstSample = park.fullName[0];
+    charts(firstSample);
+    demo(firstSample);
+  });
+}
