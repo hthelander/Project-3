4edbabbb12ce32d6071eaa2a1c95af02f9c486a1
Colleen-Cobb <colleen.cobb5@gmail.com>
2023-01-16 20:01:55 -0500

    updated activity map

--- static/js/activities.js
+++ static/js/activities.js
@@ -1,89 +1,133 @@
+// Perform an API call to the Local NPS Flask API to get the park information. Call createMarkers when it completes.
+d3.json("http://127.0.0.1:5000/api/v1.0/parks").then(function (parksResponse) {
 
+  var parks = parksResponse
 
-function createMap(natlParks) {
+      console.log(parks)
+      
 
   // Create the tile layer that will be the background of our map.
-  var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
+     var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+     });
+
+    var topomap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
 
   // Create a baseMaps object to hold the streetmap layer.
-  var baseMaps = {
-    "Street Map": streetmap
-  };
+    var baseMaps = {
+      "Street Map": streetmap,
+      "Topography": topomap
+    };
+
+  // Create layers for the map
+    var layers = {
+      PARKS: new L.layerGroup(),
+      ACTIVITIES: new L.layerGroup(),
+      COSTS: new L.layerGroup()
+    }
 
   // Create an overlayMaps object to hold the natlParks layer.
-  var overlayMaps = {
-    "National Parks": natlParks
-  };
+    var overlayMaps = {
+      "National Parks": layers.PARKS,
+      "Activities": layers.ACTIVITIES,
+      "Entrance Fee": layers.COSTS
+    };
 
   // Create the map object with options.
-  var map = L.map("map-id", {
-    center: [40.73, -74.0059],
-    zoom: 4,
-    layers: [streetmap, natlParks]
-  });
+    var map = L.map("map-id", {
+      center: [40.73, -94.0059],
+      zoom: 4,
+      layers: [
+        layers.PARKS,
+        layers.ACTIVITIES,
+        layers.COSTS
+      ]
+    });
 
-  var marker = ({
-    markerColor: "green",
-    shape: "penta"
-  });
+    streetmap.addTo(map);
 
   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(map);
-}
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(map);
 
-function createMarkers(response) {
-
-  console.log(response)
-
-  console.log(response[0].description)
-
-  // Pull the "park" information from response.data.
-  var parks = response
-
-  console.log(parks[0].longitude)
-
-  var activity = response
-  console.log(activity[0].activity)
-
-  // Initialize an array to hold park markers.
-  var parkMarkers = [];
-  var activityMarkers = [];
-  
-  
-  
-  // Loop through the park array.
-  for (var index = 0; index < parks.length; index++) {
-
-    var park = parks[index];
-
-    // For each park, create a marker, and bind a popup with the park's name and activity.
-    // var parkMarker = L.marker({markerColor: "green", shape: "penta"})([park.latitude, park.longitude])
-    // .bindPopup("<h3>" + park.fullName + "<h3> <h6>" + park.activity + "</h6>");
-    var parkMarker = L.marker([park.latitude, park.longitude])
-    .bindPopup("<h3><strong>" + park.fullName + "<strong><h3> <h6>" + park.activity + "</h6>");
-      // .bindPopup("<h3>" + park.fullName + "<h3>"),
-      // activityMarker = L.marker([park.latitude, park.longitude])
-      // .bindPopup("<h6>" + park.activity + "</h6>");
+    var parkClusters = L.markerClusterGroup()
+    var activityClusters = L.markerClusterGroup()
+    var costClusters = L.markerClusterGroup()
 
 
-      // .bindPopup("<h3>" + park.fullName + "<h3> <h6>" + park.activity + "</h6>");
+    var parkMarkers = [];
+    var activityMarkers = [];
+    var costMarkers = [];
 
-   
-    // Add the marker to the parkMarkers array.
-    parkMarkers.push(parkMarker);
-    // parkMarkers.push(parkMarker, activityMarker);
-    // activityMarkers.push(activityMarker);
-  }
+    var parkIcon = L.ExtraMarkers.icon({
+      icon: "ion-ios-navigate",
+      iconColor: "white",
+      markerColor: "blue",
+      shape: "penta"
+      })
 
-  // Create a layer group that's made from the park markers array, and pass it to the createMap function.
-  createMap(L.layerGroup(parkMarkers));
-  // createMap.addlayer(activityMarkers);
-};
+    var activityIcon = L.ExtraMarkers.icon({
+      icon: "ion-help-buoy",
+      iconColor: "white",
+      markerColor: "orange",
+      shape: "circle"
+      })
+
+    var costIcon = L.ExtraMarkers.icon({
+      icon: "ion-social-usd-outline",
+      iconColor: "white",
+      markerColor: "green",
+      shape: "circle"
+      })
+
+    for (var i = 0; i < parks.length; i++) {
+      var park = parks[i];
+
+      var parkMarker = L.marker([park.latitude, park.longitude], {
+        icon: parkIcon
+      })
+        .bindPopup("<h3><strong>" + park.fullName + "<strong><h3> <h6>" + park.description + "</h6>");
+
+        parkClusters.addLayer(parkMarker)
+        parkClusters.addTo(layers.PARKS)
+
+    }
+
+    for (var i = 0; i < parks.length; i++) {
+      var activity = parks[i];
+
+      var activityMarker = L.marker([activity.latitude, activity.longitude], {
+        icon: activityIcon
+      })
+        .bindPopup("<h3><strong>" + activity.fullName + "<strong><h3> <h6>" + activity.activity + "</h6>");
+
+        activityClusters.addLayer(activityMarker)
+        activityClusters.addTo(layers.ACTIVITIES)
+
+    }
+
+    for (var i = 0; i < parks.length; i++) {
+      var cost = parks[i];
+
+      var costMarker = L.marker([cost.latitude, cost.longitude], {
+        icon: costIcon
+      })
+        .bindPopup("<h3><strong>" + cost.fullName + "<strong><h3> <h6>$" + cost.cost + "</h6>");
+
+        costClusters.addLayer(costMarker)
+        costClusters.addTo(layers.COSTS)
+
+    }
+    });
 
 
-// Perform an API call to the Local NPS Flask API to get the park information. Call createMarkers when it completes.
-d3.json("http://127.0.0.1:5000/api/v1.0/parks").then(createMarkers);
+    
+
+
+
+
+
+
